c979c9b3da962e1e97e30ba37520468968c2cefb
Hideyuki Yokoyama <yoko8ma@users.noreply.github.com>
2022-06-05 12:10:13 +0900

    add

--- arc086_a.py
+++ arc086_a.py
@@ -1,3 +1,13 @@
+import collections
+
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 print(N,K,A)
+c = collections.Counter(A)
+print(c)
+print(c.values())
+l = list(c.values())
+l.sort()
+print(l)
+print(l[-K:])
+print(sum(l) - sum(l[-K:]))
